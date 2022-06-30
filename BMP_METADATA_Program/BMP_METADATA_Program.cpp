57b02f04db09175a4121469dddc72b79cece1d25
TytuZ <xtytuspl@gmail.com>
2022-06-30 20:21:03 +0200

    Program version 1.

--- BMP_METADATA_Program/BMP_METADATA_Program.cpp
+++ BMP_METADATA_Program/BMP_METADATA_Program.cpp
@@ -1,7 +1,37 @@
-﻿#include <iostream>
+﻿#pragma pack(1)
 
-int main()
-{
-    
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#include "bmp.h"
+
+vector<char> buffer;
+PBITMAPFILEHEADER file_header;
+PBITMAPINFOHEADER info_header;
+
+int main() {
+    cout << "Wczytywanie Pliku..." << endl;
+    ifstream file("test.bmp");
+
+    if (file) {
+        cout << "Plik wczytany.\nOdczyt Danych..." << endl;
+        file.seekg(0, std::ios::end);
+        streampos length = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        buffer.resize(length);
+        file.read(&buffer[0], length);
+
+        file_header = (PBITMAPFILEHEADER)(&buffer[0]);
+        info_header = (PBITMAPINFOHEADER)(&buffer[0] + sizeof(BITMAPFILEHEADER));
+    }
+
+    cout << "Dane pliku: "<<endl;
+    cout << "Rozmiar pliku: " << file_header->bfSize << endl;
+    cout << "Wymiary pliku: " << info_header->biWidth << " " << info_header->biHeight << endl;
+
+    return 0;
 }
-
